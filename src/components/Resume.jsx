eea15603c69cb3e4a294b8b594e20971a27c4001
brijesh <brijeshkumar4705r@gmail.com>
2025-06-29 02:08:14 +0530

    finalstep

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import resume from"../../assets/resume.jpg"
 const Resume = () => {
   return (
     <div className='flex justify-center mt-44'>
         <div className="card bg-base-100 w-96 shadow-sm ">
   <figure>
     <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      src={resume}
       alt="Shoes" />
   </figure>
   
